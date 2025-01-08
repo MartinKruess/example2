ff4c48912df0a9876f86d4e9051046da8ce83810
MartinKruess <martinkr90@googlemail.com>
2025-01-08 10:25:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// if, switch, Logikoperatoren, Scope
+
+const age = 18
+const look = "uncool"
+
+// if (age < 18) {
+//     console.log("Du kommst nicht rein!")
+// } else {
+//     if (look === "cool") {
+//         console.log("Schönen Abend")
+//     }
+// }
+
+// if (age < 18 || look !== "cool") {
+//     console.log("Du kommst nicht rein!")
+// } else {
+//     console.log("Schönen Abend")
+// }
+
+// if (age > 17 && look === "cool") {
+//     console.log("Schönen Abend")
+// } else {
+//     console.log("Du kommst nicht rein!")
+// }
+
+// let name1;
+
+// switch (look) {
+//     case "cool":
+//         {
+//             name1 = "a"
+//         }
+//         break;
+//     case "uncool":
+//         {
+//             name1 = "b"
+//         }
+//         break;
+//     default:
+//         console.log("Bin mir noch nicht sicher.")
+//         break;
+// }
+
+// name1 += "c"
+// console.log(name1)
